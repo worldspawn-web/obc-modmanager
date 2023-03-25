1a1e86841d5ff3ee6ffdf96b8b44d8820e281b96
Worldspawn <worldspawn.web@gmail.com>
2023-03-25 18:52:59 +0300

    Active mods counter

---
+++ js/activecount.js
@@ -0,0 +1,3 @@
+let scoreCountHeader = document.getElementById("active-score");
+let amountOfMods = document.querySelectorAll(".container-active");
+scoreCountHeader = scoreCountHeader.textContent = amountOfMods.length;
