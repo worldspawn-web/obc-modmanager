0611e8094308bbba5bd6947f009c8ebe41c01f8c
Worldspawn <worldspawn.web@gmail.com>
2023-12-29 18:16:42 +0300

    feat: removed some old files

--- src/statsChecker.ts
+++
@@ -1,9 +0,0 @@
-export const statsChecker = (): void => {
-  const stats = document.querySelectorAll('.stats-rating');
-  stats.forEach((stat) => {
-    const rating = Number(stat.textContent);
-    if (rating > 3) stat.classList.add('text-success');
-    if (rating === 3) stat.classList.add('text-warning');
-    if (rating < 3) stat.classList.add('text-danger');
-  });
-};
