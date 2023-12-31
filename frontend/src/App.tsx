69493690a178814eca0684a44a3a7a8ab329bce0
Worldspawn <worldspawn.web@gmail.com>
2023-12-31 17:58:15 +0300

    feat: basic Suspense added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect, Suspense } from "react";
+import { Spinner } from "react-bootstrap";
 
 import Home from "./pages/Home";
 import ActiveMods from "./pages/ActiveMods.tsx";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  // TODO: FIX SUSPENSE
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="active" element={<ActiveMods />} />
-        {/* <Route path="disabled" element={<DisabledMods />} />
+      <Suspense fallback={<Spinner animation="grow" />}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="active" element={<ActiveMods />} />
+          {/* <Route path="disabled" element={<DisabledMods />} />
           <Route path="testing" element={<TestingMods />} />
           <Route path="declined" element={<DeclinedMods />} />
           <Route path="*" element={<NotFound />} /> */}
-      </Routes>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
