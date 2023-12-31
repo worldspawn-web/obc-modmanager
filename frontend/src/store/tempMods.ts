46592c37478ac3239ec741f33d25311ad5914fd4
Worldspawn <worldspawn.web@gmail.com>
2023-12-31 16:16:46 +0300

    fix: backend structure redo

--- frontend/src/store/tempMods.ts
+++ frontend/src/store/tempMods.ts
@@ -1,27 +1,45 @@
 // # TEMPORARY STORAGE FOR LANDING DEV
 // # TODO: REDO WITH REAL BACKEND
+interface Mod {
+  id: number;
+  name: string;
+  version: number | string;
+  link: string;
+  type: string;
+  filesize: number;
+  trust: string;
+  images: {
+    "front-view": string;
+    "back-view": string;
+    "interior-view": string;
+  };
+  stats: {
+    quality: number;
+    handling: number;
+    tuning: number;
+    coop: boolean;
+    size: number;
+    fps: number;
+  }[];
+}
 
-const modsStore = [
+const modsStore: Mod[] = [
   {
     id: 0,
     name: "Cadillac CT5-V Black Wing 2022",
     version: 1.49,
     link: "https://stmods.org/euro_truck_simulator_2/mods/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2/",
     type: "car",
-    workshop: false,
-    coop: true,
     filesize: 167,
     trust: "New",
-    images: [
-      {
-        "front-view":
-          "https://stmods.org/euro_truck_simulator_2/mods/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2_img1.jpg?",
-        "back-view":
-          "https://stmods.org/euro_truck_simulator_2/mods/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2_img2.jpg?",
-        "interior-view":
-          "https://stmods.org/euro_truck_simulator_2/mods/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2_img5.jpg?",
-      },
-    ],
+    images: {
+      "front-view":
+        "https://stmods.org/euro_truck_simulator_2/mods/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2_img1.jpg?",
+      "back-view":
+        "https://stmods.org/euro_truck_simulator_2/mods/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2_img2.jpg?",
+      "interior-view":
+        "https://stmods.org/euro_truck_simulator_2/mods/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2_img5.jpg?",
+    },
     stats: [
       {
         quality: 5,
@@ -29,29 +47,26 @@ const modsStore = [
         tuning: 3,
         coop: true,
         size: 3,
+        fps: 5,
       },
     ],
   },
   {
     id: 1,
     name: "Cadillac  Escalade 2021",
-    version: 1.49,
+    version: "Steam",
     link: "https://stmods.org/euro_truck_simulator_2/mods/cadillac_escalade_2021_by_dn_modding_for_ets2/",
     type: "car",
-    workshop: false,
-    coop: true,
     filesize: 220,
     trust: "Trusted",
-    images: [
-      {
-        "front-view":
-          "https://stmods.org/euro_truck_simulator_2/mods/cadillac_escalade_2021_by_dn_modding_for_ets2/cadillac_escalade_2021_by_dn_modding_for_ets2_img1.jpg?",
-        "back-view":
-          "https://stmods.org/euro_truck_simulator_2/mods/cadillac_escalade_2021_by_dn_modding_for_ets2/cadillac_escalade_2021_by_dn_modding_for_ets2_img2.jpg?",
-        "interior-view":
-          "https://stmods.org/euro_truck_simulator_2/mods/cadillac_escalade_2021_by_dn_modding_for_ets2/cadillac_escalade_2021_by_dn_modding_for_ets2_img5.jpg?",
-      },
-    ],
+    images: {
+      "front-view":
+        "https://stmods.org/euro_truck_simulator_2/mods/cadillac_escalade_2021_by_dn_modding_for_ets2/cadillac_escalade_2021_by_dn_modding_for_ets2_img1.jpg?",
+      "back-view":
+        "https://stmods.org/euro_truck_simulator_2/mods/cadillac_escalade_2021_by_dn_modding_for_ets2/cadillac_escalade_2021_by_dn_modding_for_ets2_img2.jpg?",
+      "interior-view":
+        "https://stmods.org/euro_truck_simulator_2/mods/cadillac_escalade_2021_by_dn_modding_for_ets2/cadillac_escalade_2021_by_dn_modding_for_ets2_img5.jpg?",
+    },
     stats: [
       {
         quality: 4,
@@ -59,6 +74,7 @@ const modsStore = [
         tuning: 2,
         coop: true,
         size: 3,
+        fps: 5,
       },
     ],
   },
