1a7b2742ca8ad6b6877b73ef4c72dc7c03970b86
Worldspawn <worldspawn.web@gmail.com>
2023-12-31 04:02:38 +0300

    fix: temp store changed to simple ts file (will change to backend way later)

---
+++ frontend/src/store/tempMods.ts
@@ -0,0 +1,67 @@
+// # TEMPORARY STORAGE FOR LANDING DEV
+// # TODO: REDO WITH REAL BACKEND
+
+const modsStore = [
+  {
+    id: 0,
+    name: "Cadillac CT5-V Black Wing 2022",
+    version: 1.49,
+    link: "https://stmods.org/euro_truck_simulator_2/mods/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2/",
+    type: "car",
+    workshop: false,
+    coop: true,
+    filesize: 167,
+    trust: "New",
+    images: [
+      {
+        "front-view":
+          "https://stmods.org/euro_truck_simulator_2/mods/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2_img1.jpg?",
+        "back-view":
+          "https://stmods.org/euro_truck_simulator_2/mods/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2_img2.jpg?",
+        "interior-view":
+          "https://stmods.org/euro_truck_simulator_2/mods/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2/cadillac_ct5_v_black_wing_2022_by_dn_modding_for_ets2_img5.jpg?",
+      },
+    ],
+    stats: [
+      {
+        quality: 5,
+        handling: 4,
+        tuning: 3,
+        coop: true,
+        size: 3,
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "Cadillac  Escalade 2021",
+    version: 1.49,
+    link: "https://stmods.org/euro_truck_simulator_2/mods/cadillac_escalade_2021_by_dn_modding_for_ets2/",
+    type: "car",
+    workshop: false,
+    coop: true,
+    filesize: 220,
+    trust: "Trusted",
+    images: [
+      {
+        "front-view":
+          "https://stmods.org/euro_truck_simulator_2/mods/cadillac_escalade_2021_by_dn_modding_for_ets2/cadillac_escalade_2021_by_dn_modding_for_ets2_img1.jpg?",
+        "back-view":
+          "https://stmods.org/euro_truck_simulator_2/mods/cadillac_escalade_2021_by_dn_modding_for_ets2/cadillac_escalade_2021_by_dn_modding_for_ets2_img2.jpg?",
+        "interior-view":
+          "https://stmods.org/euro_truck_simulator_2/mods/cadillac_escalade_2021_by_dn_modding_for_ets2/cadillac_escalade_2021_by_dn_modding_for_ets2_img5.jpg?",
+      },
+    ],
+    stats: [
+      {
+        quality: 4,
+        handling: 3,
+        tuning: 2,
+        coop: true,
+        size: 3,
+      },
+    ],
+  },
+];
+
+export default modsStore;
