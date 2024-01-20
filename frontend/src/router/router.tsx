ee87f9f568914515f48ae4365d292004a771d417
Worldspawn <worldspawn.web@gmail.com>
2024-01-20 19:27:06 +0300

    feat: router init

---
+++ frontend/src/router/router.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { routenames } from './routenames';
+import MainPage from '../pages/MainPage';
+
+export const Router: FC = () => (
+  <Routes>
+    <Route path={routenames.HOME} element={<MainPage />} />
+    {/* <Route path={routenames.ETS2} element={} /> */}
+  </Routes>
+);
