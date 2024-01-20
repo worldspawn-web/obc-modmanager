279647bf97a38aa10c0d42fc0c5ac0ef40feb728
Worldspawn <worldspawn.web@gmail.com>
2024-01-20 19:28:53 +0300

    feat: mods data will be stored locally for now

---
+++ frontend/src/data/mods.ts
@@ -0,0 +1,116 @@
+const modsStore = {
+  ets2: [
+    {
+      id: 0,
+      type: 'car',
+      name: 'Bentley Bentayga Startech 2017',
+      link: 'https://stmods.org/euro_truck_simulator_2/mods/bentley_bentayga_startech_2017_by_dn_modding_for_ets2/',
+      author: 'DN Modding',
+      trusted: true,
+      version: '1.49',
+      status: 'Active',
+      stats: [
+        {
+          handling: 5,
+          quality: 5,
+          tuning: 5,
+          animations: 5,
+          fps: 5,
+          size: 5,
+          coop: true,
+        },
+      ],
+      images: ['', '', ''],
+    },
+  ],
+  beamng: [
+    {
+      id: 0,
+      type: null,
+      name: '',
+      link: '',
+      author: '',
+      version: '',
+      status: '',
+      stats: [
+        {
+          handling: 5,
+          quality: 5,
+          tuning: 5,
+          animations: 5,
+          fps: 5,
+          size: 5,
+          coop: true,
+        },
+      ],
+      images: ['', '', ''],
+    },
+  ],
+  gmod: [
+    {
+      id: 0,
+      type: null,
+      name: '',
+      link: '',
+      author: '',
+      version: '',
+      description: '',
+      status: '',
+      stats: [
+        {
+          quality: 5,
+          fps: 5,
+          size: 5,
+          coop: true,
+        },
+      ],
+      images: ['', '', '', '', ''],
+    },
+  ],
+  lethalcompany: [
+    {
+      id: 0,
+      name: '',
+      link: '',
+      author: '',
+      version: '',
+      description: '',
+      status: '',
+      stats: [
+        {
+          fps: 5,
+          size: 5,
+          coop: true,
+        },
+      ],
+      images: ['', '', ''],
+    },
+  ],
+  other: [
+    {
+      id: 0,
+      name: '',
+      game: '',
+      link: '',
+      author: '',
+      version: '',
+      status: '',
+      image: '',
+      description: '',
+    },
+  ],
+  software: [
+    {
+      id: 0,
+      name: '',
+      link: '',
+      author: '',
+      version: '',
+      status: '',
+      image: '',
+      description: '',
+    },
+  ],
+};
+
+export default modsStore;
