1a2231bc3d5747eeb1458faf1c308227a01b01ca
Worldspawn <worldspawn.web@gmail.com>
2024-01-26 19:35:03 +0300

    feat: added a couple of active mods

--- frontend/src/data/mods.ts
+++ frontend/src/data/mods.ts
@@ -11,9 +11,78 @@ const modsStore = {
       status: 'Active',
       stats: [
         {
+          handling: 3,
+          quality: 5,
+          tuning: 1,
+          animations: 4,
+          fps: 5,
+          size: 2,
+          coop: true,
+        },
+      ],
+      image:
+        'https://stmods.org/euro_truck_simulator_2/mods/bentley_bentayga_startech_2017_by_dn_modding_for_ets2/bentley_bentayga_startech_2017_by_dn_modding_for_ets2_img1.jpg?',
+    },
+    {
+      id: 1,
+      type: 'car',
+      name: 'Honda Titan 2008',
+      link: 'https://stmods.org/euro_truck_simulator_2/mods/honda_titan_2008_by_quality3d_mods_for_ets2/',
+      author: 'Quality3D Mods',
+      trusted: false,
+      version: '1.49',
+      status: 'Active',
+      stats: [
+        {
           handling: 5,
           quality: 5,
           tuning: 5,
+          animations: 5,
+          fps: 5,
+          size: 5,
+          coop: false,
+        },
+      ],
+      image:
+        'https://stmods.org/euro_truck_simulator_2/mods/honda_titan_2008_by_quality3d_mods_for_ets2/honda_titan_2008_by_quality3d_mods_for_ets2_img2.jpg?',
+    },
+    {
+      id: 2,
+      type: 'car',
+      name: 'Porsche 911 Turbo S 2016',
+      link: 'https://stmods.org/euro_truck_simulator_2/mods/porsche_911_turbo_s_2016_by_mert_ir_i_for_ets2/',
+      author: 'Mert İrşi',
+      trusted: true,
+      version: '1.49',
+      status: 'Active',
+      stats: [
+        {
+          handling: 4,
+          quality: 5,
+          tuning: 1,
+          animations: 4,
+          fps: 5,
+          size: 4,
+          coop: true,
+        },
+      ],
+      image:
+        'https://stmods.org/euro_truck_simulator_2/mods/porsche_911_turbo_s_2016_by_mert_ir_i_for_ets2/porsche_911_turbo_s_2016_by_mert_ir_i_for_ets2_img1.jpg?',
+    },
+    {
+      id: 3,
+      type: 'car',
+      name: 'Bollore Bluebus SE',
+      link: 'https://stmods.org/euro_truck_simulator_2/mods/bollore_bluebus_se_by_etrusan_for_ets2/',
+      author: 'Etrusan',
+      trusted: true,
+      version: '1.49',
+      status: 'Disabled',
+      stats: [
+        {
+          handling: 5,
+          quality: 5,
+          tuning: 2,
           animations: 5,
           fps: 5,
           size: 5,
@@ -21,7 +90,30 @@ const modsStore = {
         },
       ],
       image:
-        'https://stmods.org/euro_truck_simulator_2/mods/bentley_bentayga_startech_2017_by_dn_modding_for_ets2/bentley_bentayga_startech_2017_by_dn_modding_for_ets2_img1.jpg?',
+        'https://stmods.org/euro_truck_simulator_2/mods/bollore_bluebus_se_by_etrusan_for_ets2/bollore_bluebus_se_v1_0_11_46_by_etrusan_for_ets2_v1_45_x_1_46_x_img1.jpg?6550df8162011',
+    },
+    {
+      id: 4,
+      type: 'car',
+      name: 'Porsche Cayenne Turbo 2022',
+      link: 'https://stmods.org/euro_truck_simulator_2/mods/porsche_cayenne_turbo_2022_by_dn_modding_for_ets2/',
+      author: 'DN Modding',
+      trusted: true,
+      version: '1.49',
+      status: 'Active',
+      stats: [
+        {
+          handling: 5,
+          quality: 5,
+          tuning: 1,
+          animations: 4,
+          fps: 5,
+          size: 4,
+          coop: true,
+        },
+      ],
+      image:
+        'https://stmods.org/euro_truck_simulator_2/mods/porsche_cayenne_turbo_2022_by_dn_modding_for_ets2/porsche_cayenne_turbo_2022_by_dn_modding_for_ets2_img3.jpg?6590342a2db3d',
     },
   ],
   beamng: [
