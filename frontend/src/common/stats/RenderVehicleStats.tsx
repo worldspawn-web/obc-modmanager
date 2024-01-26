c21d67eac836ade20fa773deb9e59f0e08cd3ff7
Worldspawn <worldspawn.web@gmail.com>
2024-01-26 19:35:41 +0300

    feat: new vehicle stats modal

--- frontend/src/common/stats/RenderVehicleStats.tsx
+++ frontend/src/common/stats/RenderVehicleStats.tsx
@@ -1,20 +1,51 @@
-import { Flex } from '@radix-ui/themes';
+import { Flex, Table } from '@radix-ui/themes';
 import { RatingMark, FailureMark } from '..';
 
 // TODO: change ANY type
 const RenderVehicleStats = (stats: any) => {
   return (
-    <section>
-      <Flex gap="3">Handling: {stats[0].handling}</Flex>
-      <Flex gap="3">Quality: {stats[0].quality}</Flex>
-      <Flex gap="3">Tuning: {stats[0].tuning}</Flex>
-      <Flex gap="3">Animations: {stats[0].animations}</Flex>
-      <Flex gap="3">FPS: {stats[0].fps}</Flex>
-      <Flex gap="3">Size: {stats[0].size}</Flex>
-      <Flex gap="3">
-        Coop: {stats[0].coop ? <RatingMark /> : <FailureMark />}
-      </Flex>
-    </section>
+    <Flex direction="column" gap="3">
+      <Table.Root>
+        <Table.Body>
+          <Table.Row>
+            <Table.RowHeaderCell>Handling</Table.RowHeaderCell>
+            <Table.Cell>{stats[0].handling}</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.RowHeaderCell>Quality</Table.RowHeaderCell>
+            <Table.Cell>{stats[0].quality}</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.RowHeaderCell>Tuning</Table.RowHeaderCell>
+            <Table.Cell>{stats[0].tuning}</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.RowHeaderCell>Animations</Table.RowHeaderCell>
+            <Table.Cell>{stats[0].animations}</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.RowHeaderCell>FPS</Table.RowHeaderCell>
+            <Table.Cell>{stats[0].fps}</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.RowHeaderCell>Size</Table.RowHeaderCell>
+            <Table.Cell>{stats[0].size}</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.RowHeaderCell>Coop</Table.RowHeaderCell>
+            <Table.Cell>
+              {stats[0].coop ? <RatingMark /> : <FailureMark />}
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table.Root>
+    </Flex>
   );
 };
 
