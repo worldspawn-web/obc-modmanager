523e2ffa8cfc605322faf6c701226dddb6c8c380
Worldspawn <worldspawn.web@gmail.com>
2024-01-28 17:21:43 +0300

    feat: carousel images bug fix

--- frontend/src/common/carousel/ImagesCarousel.tsx
+++ frontend/src/common/carousel/ImagesCarousel.tsx
@@ -5,21 +5,33 @@ export const ImagesCarousel = () => {
   return (
     <Carousel className={styles.carousel}>
       <Carousel.Item className={styles.carousel__image}>
-        <img src="../public/banner.jpg" alt="123" />
+        <img
+          src="../public/banner.jpg"
+          alt="123"
+          className={styles.carousel__img}
+        />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className={styles.carousel__image}>
-        <img src="../public/ets2_background.jpg" alt="123" />
+        <img
+          src="../public/ets2_background.jpg"
+          alt="123"
+          className={styles.carousel__img}
+        />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className={styles.carousel__image}>
-        <img src="../public/banner.jpg" alt="123" />
+        <img
+          src="../public/banner.jpg"
+          alt="123"
+          className={styles.carousel__img}
+        />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
