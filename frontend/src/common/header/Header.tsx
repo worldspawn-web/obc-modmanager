ddbbe872b26ef945e134772577fb66ed975c7c5d
Worldspawn <worldspawn.web@gmail.com>
2024-01-28 17:21:09 +0300

    feat: header text converted to a nice logo banner

--- frontend/src/common/header/Header.tsx
+++ frontend/src/common/header/Header.tsx
@@ -1,11 +1,15 @@
 import { Flex } from '@radix-ui/themes';
 import { OBCLogo } from '..';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <Flex gap="3" justify="between" align="center" mt="3">
-      <OBCLogo />
-      <span>OBC ModManager</span>
+      <Link to="/">
+        <OBCLogo />
+      </Link>
+
+      <img src="../src/icons/logo-long.png" />
     </Flex>
   );
 };
