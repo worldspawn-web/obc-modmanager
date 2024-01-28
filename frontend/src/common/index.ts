946ea5de103c79fd108cc65c6d245168302f6cfe
Worldspawn <worldspawn.web@gmail.com>
2024-01-28 17:23:28 +0300

    feat: indexation

--- frontend/src/common/index.ts
+++ frontend/src/common/index.ts
@@ -1,3 +1,5 @@
 export * from './icons';
 export * from './header';
 export * from './carousel';
+export * from './footer';
+export * from './spinners';
---
+++ frontend/src/common/spinners/index.ts
@@ -0,0 +1,1 @@
+export { SpinnerSuspense } from './SpinnerSuspense';
