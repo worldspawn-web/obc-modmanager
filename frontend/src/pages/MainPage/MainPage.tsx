9d75ebd1481a746b3e85e3e57e24b73162f9d2b1
Worldspawn <worldspawn.web@gmail.com>
2024-01-28 17:22:12 +0300

    feat: added footer as component

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,78 +1,82 @@
-import { Container, Grid, Heading } from '@radix-ui/themes';
+import { Container, Grid, Heading, Link } from '@radix-ui/themes';
 
 import styles from './MainPage.module.scss';
 import { Header } from '../../common/header/Header';
+import { Footer } from '../../common/footer/Footer';
 
 export const MainPage = () => {
   return (
-    <div className={styles.page}>
-      <Container>
-        <Header />
-        <Container pt="6">
-          <Heading align="center" mb="6">
-            Welcome to the OBC Mod Manager!
-          </Heading>
-          <Grid
-            columns="4"
-            gap="8"
-            width="auto"
-            align="center"
-            justify="center"
-            className={styles.categories}
-            mt="8"
-            mb="8"
-          >
-            <a href="/ets2">
-              <img
-                className={styles.category}
-                src="../src/icons/ets.png"
-                alt="Euro Truck Simulator 2"
-              />
-            </a>
+    <>
+      <div className={styles.page}>
+        <Container>
+          <Header />
+          <Container pt="6">
+            <Heading align="center" mb="6">
+              Welcome to the OBC Mod Manager!
+            </Heading>
+            <Grid
+              columns="4"
+              gap="8"
+              width="auto"
+              align="center"
+              justify="center"
+              className={styles.categories}
+              mt="9"
+              mb="9"
+            >
+              <Link href="/ets2">
+                <img
+                  className={styles.category}
+                  src="../src/icons/ets.png"
+                  alt="Euro Truck Simulator 2"
+                />
+              </Link>
 
-            <a href="/beamng">
-              <img
-                className={styles.category}
-                src="../src/icons/bea33mng.png"
-                alt="BeamNG"
-              />
-            </a>
+              <Link href="/beamng">
+                <img
+                  className={styles.category}
+                  src="../src/icons/bea33mng.png"
+                  alt="BeamNG"
+                />
+              </Link>
 
-            <a href="/gmod">
-              <img
-                className={styles.category}
-                src="../src/icons/gmod1.png"
-                alt="Garry's Mod"
-              />
-            </a>
+              <Link href="/gmod">
+                <img
+                  className={styles.category}
+                  src="../src/icons/gmod1.png"
+                  alt="Garry's Mod"
+                />
+              </Link>
 
-            <a href="/lethalcompany">
-              <img
-                className={styles.category}
-                src="../src/icons/lethalcompany.png"
-                alt="Lethal Company"
-              />
-            </a>
+              <Link href="/lethalcompany">
+                <img
+                  className={styles.category}
+                  src="../src/icons/lethalcompany.png"
+                  alt="Lethal Company"
+                />
+              </Link>
 
-            <a href="/minecraft">
-              <img
-                className={styles.category}
-                src="../src/icons/minecraft.png"
-                alt="Minecraft"
-              />
-            </a>
+              <Link href="/minecraft">
+                <img
+                  className={styles.category}
+                  src="../src/icons/minecraft.png"
+                  alt="Minecraft"
+                />
+              </Link>
 
-            <a href="/software">
-              <img
-                className={styles.category}
-                src="../src/icons/software.png"
-                alt="Software"
-              />
-            </a>
-          </Grid>
+              <Link href="/software">
+                <img
+                  className={styles.category}
+                  src="../src/icons/software.png"
+                  alt="Software"
+                />
+              </Link>
+            </Grid>
+          </Container>
         </Container>
-      </Container>
-    </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
