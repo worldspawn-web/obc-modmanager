f4f704d7ab1756cf1b07ec185729506ce30a1b55
Worldspawn <worldspawn.web@gmail.com>
2024-01-28 19:19:19 +0300

    refactor: removed unnecessary import and comms

--- frontend/src/common/render/RenderNoMods.tsx
+++ frontend/src/common/render/RenderNoMods.tsx
@@ -1,12 +1,8 @@
 import { Callout, Link } from '@radix-ui/themes';
-import styles from './RenderAllETSMods.module.scss';
 
 export const RenderNoMods = () => {
   return (
     <Callout.Root color="blue">
-      {/* <Callout.Icon>
-          <InfoCircledIcon />
-        </Callout.Icon> */}
       <Callout.Text>
         There are currently <Link href="#">no mods</Link> in this section.
       </Callout.Text>
