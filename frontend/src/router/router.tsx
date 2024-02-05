043fb55f6caaf3a625e95982c443fa829d87c26a
Worldspawn <worldspawn.web@gmail.com>
2024-02-05 19:53:53 +0300

    feat: lazy loading for all the new pages

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -5,11 +5,23 @@ import { MainPage } from '../pages';
 
 const ETS2Page = lazy(() => import('../pages/ETS2/ETS2Page'));
 const Error404 = lazy(() => import('../pages/Error404/PageNotFound'));
+const BeamNGPage = lazy(() => import('../pages/BeamNG/BeamNGPage'));
+const GmodPage = lazy(() => import('../pages/GMOD/GmodPage'));
+const MinecraftPage = lazy(() => import('../pages/Minecraft/MinecraftPage'));
+const SoftwarePage = lazy(() => import('../pages/Software/SoftwarePage'));
+const LethalCompanyPage = lazy(
+  () => import('../pages/LethalCompany/LethalCompanyPage')
+);
 
 export const Router: FC = () => (
   <Routes>
     <Route path={routenames.HOME} element={<MainPage />} />
     <Route path={routenames.ETS2} element={<ETS2Page />} />
+    <Route path={routenames.BEAMNG} element={<BeamNGPage />} />
+    <Route path={routenames.GMOD} element={<GmodPage />} />
+    <Route path={routenames.LETHALCOMPANY} element={<LethalCompanyPage />} />
+    <Route path={routenames.MINECRAFT} element={<MinecraftPage />} />
+    <Route path={routenames.SOFTWARE} element={<SoftwarePage />} />
     <Route path={routenames.NOT_FOUND} element={<Error404 />} />
   </Routes>
 );
