e901c168f9db3ca36855159c4b9e96a4cfbe2248
Worldspawn <worldspawn.web@gmail.com>
2024-02-05 19:21:01 +0300

    feat: updated 404 page

--- frontend/src/pages/Error404/PageNotFound.tsx
+++ frontend/src/pages/Error404/PageNotFound.tsx
@@ -1,11 +1,22 @@
-import { Flex } from '@radix-ui/themes';
+import { Container, Flex } from '@radix-ui/themes';
 
 import styles from './PageNotFound.module.scss';
+import { Header } from '../../common';
 
 const PageNotFound = () => (
-  <Flex align="center" justify="center" className={styles.error404__wrapper}>
-    ERROR 404
-  </Flex>
+  <Container>
+    <Header />
+    <Flex
+      align="center"
+      justify="center"
+      direction="column"
+      className={styles.error404__wrapper}
+    >
+      <h1 className={styles.error404__header}>Whoops!</h1>
+      <p>Looks like the page doesn't exist!</p>
+      <span className={styles.error404__error}>404 Error</span>
+    </Flex>
+  </Container>
 );
 
 export default PageNotFound;
